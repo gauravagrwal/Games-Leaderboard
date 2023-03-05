f908d97f73ebb68db8efeb1b95facc598b96606a
Gaurav Agrawal <gauravag.1005@gmail.com>
2023-03-05 19:56:39 +0530

    feat: (R) fetching data from an endpoint using swr
    https://swr.vercel.app/docs/getting-started

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,20 @@
+import useSWR from 'swr';
+
+const fetcher = (...args) => fetch(...args).then(res => res.json())
+
 export default function App() {
+    const { data, error, isLoading } = useSWR('https://s3-ap-southeast-1.amazonaws.com/he-public-data/TopSellingGamesbb1c49e.json', fetcher);
+    console.info(data);
+
+    if (error)
+        return (
+            <div>failed to load</div>
+        );
+    if (isLoading)
+        return (
+            <div>loading...</div>
+        );
+
     return (
         <>
         </>
